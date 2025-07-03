06182b134d2af5ea29b916835e99ce5e77544db9
Tina-1300 <tina.xytrfgthuji1348@gmail.com>
2025-07-03 13:58:58 +0200

    Release 1.2.0

--- README.md
+++ README.md
@@ -8,6 +8,91 @@ WinSpeak is a modern, powerful, and easy-to-use C++ library for controlling spea
     <img src="./WinSpeak_example_code.png" />
 </p>
 
+---
+
+```cpp
+#include <WinSpeak/WinSpeak.hpp>
+#include <WinSpeak/AudioDeviceManager.hpp>
+#include <iostream>
+
+// g++ -O2 -Wall -Wextra -o test.exe test.cpp -lWinSpeak -lole32 -lstdc++ -luuid -lOleaut32
+
+int main(){
+
+    try {
+
+        WinSpeak::AudioDeviceManager manager;
+
+        std::vector<WinSpeak::Peripheral> devices = manager.list_devices();
+
+        std::wcout << L"List of available audio devices :\n";
+
+        for (size_t i = 0; i < devices.size(); ++i){
+
+            std::wcout << L"[" << i << L"] " << devices[i].name
+            << L" (ID: " << devices[i].id << L")\n";
+        
+        }
+
+        std::wcout << L"\nChoose the index of the device to use (0, 1, etc.) : ";
+
+        size_t choix;
+
+        std::wcin >> choix;
+
+        std::wcin.ignore();
+
+        if (choix >= devices.size()){
+
+            std::wcerr << L"Invalid index.\n";
+
+            return 1;
+
+        }
+
+        if (!manager.set_device_by_id(devices[choix].id)){
+
+            std::wcerr << L"Unable to select device.\n";
+
+            return 1;
+
+        }
+
+        WinSpeak::WinSpeak speaker(&manager);
+
+        if (speaker.is_muted()){
+
+            speaker.set_mute(false);
+
+            speaker.set_volume(100.0f);
+        }
+
+        std::wcout << L"\npress (enter) to continue\n";
+
+        std::wcin.get();
+
+        std::wcout << L"Current volume: " << speaker.get_volume() << "%\n";
+
+        std::wcout << L"Mute activation...\n";
+
+        speaker.set_mute(true);
+
+        std::wcout << L"\npress (enter) to exit\n";
+
+        std::wcin.get();
+
+    }catch (const std::exception& e){
+
+        std::wcerr << L"Error : " << e.what() << "\n";
+
+    }
+
+    return 0;
+
+}
+```
+
+<br>
 
 ## 📚 WinSpeak documentation website
 
--- src/AudioDeviceManager.cpp
+++ src/AudioDeviceManager.cpp
@@ -2,45 +2,174 @@
 #include <mmdeviceapi.h>
 #include <stdexcept>
 #include <windows.h>
+#include <initguid.h>
+#include <functiondiscoverykeys_devpkey.h>
+
 
 namespace WinSpeak {
 
     AudioDeviceManager::AudioDeviceManager() : endpointVolume(nullptr){
+
         CoInitialize(nullptr);
 
         IMMDeviceEnumerator* deviceEnumerator = nullptr;
+
         HRESULT hr = CoCreateInstance(
             __uuidof(MMDeviceEnumerator), nullptr, CLSCTX_ALL,
             __uuidof(IMMDeviceEnumerator), (void**)&deviceEnumerator);
 
         if (FAILED(hr) || !deviceEnumerator){
+
             throw std::runtime_error("Failed to create device enumerator.");
+
         }
 
         IMMDevice* defaultDevice = nullptr;
+
         hr = deviceEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &defaultDevice);
+        
         deviceEnumerator->Release();
 
         if (FAILED(hr) || !defaultDevice){
+
             throw std::runtime_error("Failed to get default audio endpoint.");
+
         }
 
         hr = defaultDevice->Activate(
             __uuidof(IAudioEndpointVolume), CLSCTX_ALL, nullptr, (void**)&endpointVolume);
+        
         defaultDevice->Release();
 
         if (FAILED(hr) || !endpointVolume){
+
             throw std::runtime_error("Failed to activate endpoint volume.");
+
         }
+
     }
 
     AudioDeviceManager::~AudioDeviceManager(){
+
         if (endpointVolume) endpointVolume->Release();
+
         CoUninitialize();
+
     }
 
     IAudioEndpointVolume* AudioDeviceManager::get_endpoint() const{
+
         return endpointVolume;
+
+    }
+
+    std::vector<Peripheral> AudioDeviceManager::list_devices() const{
+
+        std::vector<Peripheral> devices;
+
+        IMMDeviceEnumerator* enumerator = nullptr;
+
+        if (FAILED(CoCreateInstance(__uuidof(MMDeviceEnumerator), nullptr,
+            CLSCTX_ALL, IID_PPV_ARGS(&enumerator)))){
+
+            return devices;
+
+        }
+
+        IMMDeviceCollection* collection = nullptr;
+
+        if (FAILED(enumerator->EnumAudioEndpoints(eRender, DEVICE_STATE_ACTIVE, &collection))){
+            
+            enumerator->Release();
+
+            return devices;
+
+        }
+
+        UINT count = 0;
+
+        collection->GetCount(&count);
+
+        for (UINT i = 0; i < count; ++i){
+
+            IMMDevice* device = nullptr;
+
+            if (FAILED(collection->Item(i, &device))){continue;}
+
+            LPWSTR id = nullptr;
+
+            device->GetId(&id);
+
+            IPropertyStore* props = nullptr;
+
+            device->OpenPropertyStore(STGM_READ, &props);
+
+            PROPVARIANT name;
+
+            PropVariantInit(&name);
+
+            props->GetValue(PKEY_Device_FriendlyName, &name);
+
+            devices.emplace_back(id, name.pwszVal);
+
+            CoTaskMemFree(id);
+
+            PropVariantClear(&name);
+
+            props->Release();
+
+            device->Release();
+
+        }
+
+        return devices; 
+    
+    }
+
+    bool AudioDeviceManager::set_device_by_id(const std::wstring& device_id){
+
+        IMMDeviceEnumerator* enumerator = nullptr;
+
+        if (FAILED(CoCreateInstance(__uuidof(MMDeviceEnumerator), nullptr, 
+            CLSCTX_ALL, IID_PPV_ARGS(&enumerator)))){
+
+            return false;
+
+        }
+
+        IMMDevice* device = nullptr;
+
+        if (FAILED(enumerator->GetDevice(device_id.c_str(), &device))){
+
+            enumerator->Release();
+
+            return false;
+
+        }
+
+        IAudioEndpointVolume* newEndpoint = nullptr;
+
+        if (FAILED(device->Activate(__uuidof(IAudioEndpointVolume), CLSCTX_ALL,
+            nullptr, (void**)&newEndpoint))){
+
+            device->Release();
+
+            enumerator->Release();
+
+            return false;
+
+        }
+
+        if (endpointVolume){endpointVolume->Release();}
+
+        endpointVolume = newEndpoint;
+
+        device->Release();
+
+        enumerator->Release();
+
+        return true;
+        
     }
 
 }
